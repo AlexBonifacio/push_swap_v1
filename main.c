2b2f0e9d3a0849107d401db3bbbed47095e9eaea
AlexBonifacio <alex13.bonifacio@gmail.com>
2025-02-04 14:54:15 +0100

    first version radix sort

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abonifac <abonifac@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 23:32:19 by abonifac          #+#    #+#             */
-/*   Updated: 2025/02/04 01:15:11 by abonifac         ###   ########.fr       */
+/*   Updated: 2025/02/04 11:51:21 by abonifac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,6 +177,21 @@ node *find_min_node(node *list)
     return (min_node);
 }
 
+int	ft_node_index(node *list, node *lf_node)
+{
+	int pos;
+	
+	pos = 1;
+	if (!list || !lf_node)
+		return (0);
+	while (list != lf_node)
+	{
+		list = list->next;
+		pos++;
+	}
+	return (pos);
+}
+
 void	sort_two_reverse(node **list)
 {
 	int	a;
@@ -192,31 +207,89 @@ void	sort_two_reverse(node **list)
 void	sort_five(node **list_a, node **list_b)
 {
 	node	*min;
-	// int		count;
+	int		count;
 	
-	// count = 0;
-	*list_b = NULL;
 	min = find_min_node((*list_a));
-	// count = ft_nodelsize((*list_a));
 	while (ft_nodelsize((*list_a)) > 3)
 	{
+		count = ft_node_index((*list_a), min);
+		if (count > 3)
+		{
 			while ((*list_a) != min)
 				rra(list_a);
 			pb(list_a, list_b);
 			min = find_min_node((*list_a));
-		// else
-		// {
-		// 	while ((*list_a) != min)
-		// 		ra(list_a);
-		// 	pb(list_a, list_b);
-		// 	min = find_min_node((*list_a));
-		// }
+		}
+		else
+		{
+			while ((*list_a) != min)
+				ra(list_a);
+			pb(list_a, list_b);
+			min = find_min_node((*list_a));
+		}
 	}
 	sort_three(list_a);
 	pa(list_a, list_b);
 	pa(list_a, list_b);
 	return ;
 }
+
+
+
+
+int	find_max_index(node *list)
+{
+	int max;
+
+	if (!list)
+		return (0);
+	max = list->nb;
+	while (list)
+	{
+		if (list->nb > max)
+			max = list->nb;
+		list = list->next;
+	}
+	return (max);
+}
+
+void	radix_sort(node **list_a, node **list_b, int size)
+{
+	int max;
+	int max_bits;
+	int i;
+	int j;
+	int num;
+
+	// On part du principe que ta pile A contient des indices allant de 0 à size - 1
+	max = find_max_index(*list_a);
+	max_bits = 0;
+	while ((max >> max_bits) != 0)
+		max_bits++; // # Résultat attendu : nombre de bits nécessaire pour représenter max
+
+	i = 0;
+	while (i < max_bits)
+	{
+		j = 0;
+		// Pour chaque passe, on parcourt tous les éléments de la pile A
+		while (j < size)
+		{
+			// Récupération de la valeur (l'indice) en tête de pile A
+			num = (*list_a)->nb;
+			// Vérifie le bit i de num : s'il est 0, on le pousse dans B, sinon on fait une rotation sur A
+			if (((num >> i) & 1) == 0)
+				pb(list_a, list_b); // # Résultat attendu : l'élément est déplacé dans la pile B
+			else
+				ra(list_a); // # Résultat attendu : la pile A est tournée
+			j++;
+		}
+		// Récupère tous les éléments de la pile B dans A
+		while (*list_b != NULL)
+			pa(list_a, list_b);
+		i++;
+	}
+}
+
 
 int	ft_select_algo(node **list_a, node **list_b, int a)
 {
@@ -226,10 +299,12 @@ int	ft_select_algo(node **list_a, node **list_b, int a)
 	{
 		sort_three(list_a);
 	}
-	else
+	else if (a > 3 && a <= 5)
 	{
 		sort_five(list_a, list_b);
 	}
+	else
+		radix_sort(list_a, list_b, a);
 	return (0);
 }
 
